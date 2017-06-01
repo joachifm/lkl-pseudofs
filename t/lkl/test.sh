60ab6d465cc9c1d8c807cc319f806128d005b170
joachifm <joachifm@fastmail.fm>
2017-06-01 18:46:03 +0200

    t/lkl/test.sh: increase verbosity

--- t/lkl/test.sh
+++ t/lkl/test.sh
@@ -1,6 +1,13 @@
 #! /bin/sh
 cd "$(dirname "$0")"
+
+echo "Creating empty image ..." >&2
 truncate -s 128M fs.img
 yes | mkfs.ext4 -q fs.img
 truncate -s 100M large.file
-$(nix-build ../../lkl --no-out-link) < ./fs.spec
+
+echo "Building ..." >&2
+prog=$(nix-build ../../lkl --no-out-link)
+
+echo "Running test ..." >&2
+$prog < ./fs.spec
