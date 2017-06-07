0585a10cf6c0196eaf87f13f1d6a5b01d8fa6edd
joachifm <joachifm@fastmail.fm>
2017-06-07 02:26:59 +0200

    More detailed test script output

--- t/lkl/test.sh
+++ t/lkl/test.sh
@@ -6,12 +6,17 @@ cd "$(dirname "$0")"
 echo "Building ..." >&2
 prog=$(nix-build ../../lkl --no-out-link)
 
-echo "Running test ..." >&2
+echo "Preparing root fs image ..." >&2
 truncate -s 100M large.file
 truncate -s 128M root.img
 yes | mkfs.ext4 -q root.img
+
+echo "Preparing boot fs image ..." >&2
+truncate -s 64M boot.img
+yes | mkfs.vfat -F32 boot.img >/dev/null
+
+echo "Testing root fs creation ..." >&2
 $prog -t ext4 -P 0 -i root.img < ./root.spec
 
-truncate -s 64M boot.img
-yes | mkfs.vfat -F32 boot.img
+echo "Testing boot fs creation ..." >&2
 $prog -t vfat -P 0 -i boot.img < ./boot.spec
