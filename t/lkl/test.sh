db2d25ece0230bf91d8c3e0de4b8110ec58494e5
joachifm <joachifm@fastmail.fm>
2017-06-19 20:27:56 +0200

    t/lkl: mark test data files nodump/nocow

--- t/lkl/test.sh
+++ t/lkl/test.sh
@@ -7,11 +7,15 @@ echo "Building ..." >&2
 prog=$(nix-build ../../lkl --no-out-link)
 
 echo "Preparing root fs image ..." >&2
+touch large.file root.img
+chattr +dC large.file root.img
 truncate -s 100M large.file
 truncate -s 128M root.img
 yes | mkfs.ext4 -q root.img
 
 echo "Preparing boot fs image ..." >&2
+touch boot.img
+chattr +dC boot.img
 truncate -s 64M boot.img
 yes | mkfs.vfat -F32 boot.img >/dev/null
 
